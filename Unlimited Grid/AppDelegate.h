be123675c1e4bb1f960978c131114c663811079a
Kenneth Friedman <kennethshawfriedman@gmail.com>
2014-07-14 17:39:26 -0400

    xCode Project Added
    Fist commit with actual code included!

---
+++ Unlimited Grid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Unlimited Grid
+//
+//  Created by Kenny Friedman on 7/14/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Unlimited Grid/StartVC.h
@@ -0,0 +1,13 @@
+//
+//  StartVC.h
+//  Unlimited Grid
+//
+//  Created by Kenny Friedman on 7/14/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartVC : UIViewController
+
+@end
