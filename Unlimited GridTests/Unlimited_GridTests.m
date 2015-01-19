78ac67a624c4ee42f1f13bcc09ac7ca90732aac9
Kenneth Friedman <Kenny@Kenneths-MacBook-Pro.local>
2015-01-19 04:21:02 -0500

    Preview Image Added

--- Unlimited GridTests/Unlimited_GridTests.m
+++ Unlimited GridTests/Unlimited_GridTests.m
@@ -2,10 +2,11 @@
 //  Unlimited_GridTests.m
 //  Unlimited GridTests
 //
-//  Created by Kenny Friedman on 7/14/14.
-//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//  Created by Kenneth Friedman on 1/18/15.
+//  Copyright (c) 2015 Kenneth Friedman. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
 @interface Unlimited_GridTests : XCTestCase
@@ -14,21 +15,26 @@
 
 @implementation Unlimited_GridTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
 }
 
 @end
